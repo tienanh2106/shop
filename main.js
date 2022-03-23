9ca04b93c4b3e17c86223ca81d653cea2a6b9bdb
tienanh2106 <dotienanh21062001@gmail.com>
2022-03-23 12:15:03 +0700

    update

---
+++ main.js
@@ -0,0 +1,16 @@
+function register_onclick_block () {
+    document.getElementById('register').style.display= 'block';
+}
+
+function register_onclick_none () {
+    document.getElementById('register').style.display= 'none';
+}
+
+function login_onclick_block () {
+    document.getElementById('login').style.display= 'block';
+}
+
+function login_onclick_none () {
+    document.getElementById('login').style.display= 'none';
+}
+
